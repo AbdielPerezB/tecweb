af651471b973c8294b637d6c6c60915f6677c6c8
AbdielPerezB <abdiel2012p@gmail.com>
2022-09-09 18:30:46 -0500

    ejercicio 1 de la pŕactica 2 realizado

--- practicas/p02/p02.php
+++ practicas/p02/p02.php
@@ -10,6 +10,26 @@
        <title>Pr&aacute;ctica 2 - Variables PHP</title>
     </head>
     <body>
+        <h2>Determina cuál de las siguientes variables son válidas y explica por qué:</h2>
+        <p>
+            $_myvar; //válida porque comienza con underscore<br>
+            $_7var; //válida porque comienza con un underscore<br>
+            myvar;  //no válida porque falta el símbolo '$'<br>
+            $myvar; //válida porque comienza con una letra<br>
+            $var7;  //válida porque comienza con una letra<br>
+            $_element1; //válida porque comienza con underscore<br>
+            $house*5;   //no válida por contener el '*'<br>
+        </p>
+        <?php
+        $_myvar;
+        $_7var;
+        myvar;
+        $myvar;
+        $var7;
+        $_element1;
+        $house*5;
+        ?>
+        <hr>
       
     </body>
 </html>
