8a39c2a20cefd0cd1e3bb835ad7294ed36eb7c2b
AbdielPerezB <abdiel2012p@gmail.com>
2022-10-04 20:28:56 -0500

    ejercicio 1 de p04 realizado

--- practicas/p04/src/funciones.php
+++ practicas/p04/src/funciones.php
@@ -1,8 +1,11 @@
 <?php
 
 function multiplo5y7($numero) {
-    // AQUÍ VA EL CÓDIGO DE LA FUNCIÓN
-    return 'respuesta: '.$numero;
+    if(($numero%5 == 0) && ($numero%7 == 0)){
+        return $numero.' si es múltiplo de 5 y 7';
+    }else{
+        return $numero.' NO es múltiplo de 5 y 7';
+    }
 }
 
 ?>
